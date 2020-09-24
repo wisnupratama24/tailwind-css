0df3d6c085512fc3c40695c329160fce74800ab4
wisnupratama24 <wisnuputrapratama24@gmail.com>
2020-09-24 23:47:39 +0700

    upload files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
